93f2e16a28af72a7abb959db5bbf9a8b634532a6
PixiePal <orsolya.tothgati@gmail.com>
2018-11-17 23:26:31 +0100

    Adds gallery buttons

--- src/components/gallery/Item.js
+++ src/components/gallery/Item.js
@@ -7,7 +7,7 @@ class Item extends Component {
       <div className='galleryItem' >
         <img src={this.props.item.imageUrl} className='galleryItemImage' alt='Raum' />
         <p className='galleryItemTitle' >{this.props.item.name}</p>
-        <button>DETAILS</button>
+        <a className='button'>DETAILS</a>
       </div>
     )
   }
