6b911eb1f3fc4cda834c537c7a87fe253e2f875a
PixiePal <orsolya.tothgati@gmail.com>
2018-11-27 23:42:06 +0100

    Adds unformatted expose data

--- src/components/expose/Expose.js
+++ src/components/expose/Expose.js
@@ -13,17 +13,69 @@ class Expose extends Component {
 
   render () {
     let roomData = this.props.details[this.state.id - 1]
-    let link = 'https://my.matterport.com/show/?m=' + roomData.model_id
+    let model = 'https://my.matterport.com/show/?m=' + roomData.model_id
+    let website = 'https://' + roomData.website
 
     return (
-      <Fragment>
-
-        <h1>{roomData.name}</h1>
+      <section className='container'>
         <iframe width='100%' height='480'
           title={roomData.id}
-          src={link}
+          src={model}
           frameBorder='0' allowFullScreen allow='vr' />
-      </Fragment>
+
+        <h1 className='title'>{roomData.name}</h1>
+
+        <p>{roomData.description}</p>
+
+        <div className='row'>
+          <div className='oneColumn'>
+            <h5>Ausstattung</h5>
+            {roomData.equipment_installed.map((item, index) => (
+              <span>{item}</span>
+            ))}
+            <br />
+            <p>Auf Anfrage: </p>
+            {roomData.equipment_optional.map((item, index) => (
+              <span>{item}</span>
+            ))}
+          </div>
+          <div className='oneColumn'>
+            <h5>Kapazität</h5>
+            <p>Fläche: {roomData.area}</p>
+            {roomData.seating.map((item, index) => (
+              <p>{item.type}: bis zu <strong>{item.max}</strong> Personen</p>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h3>Bewertungen:</h3>
+          {roomData.reviews.map((item, index) => (
+            <p><strong>{item.stars}</strong> {item.text} ({item.datum}) </p>
+          ))}
+        </div>
+
+        <div>
+          <h3>Ihre Ansprechpartner: </h3>
+          <p>{roomData.contact.name}</p>
+          <p>{roomData.contact.email}</p>
+          <p>{roomData.tel}</p>
+          <p>{roomData.fax}</p>
+          <form>
+            <p>Name: <input /></p>
+            <p>Nachricht: <input /></p>
+            <button type='submit' >Absenden</button>
+          </form>
+        </div>
+
+        <div>
+          Placeholder for google maps {roomData.map_code} + {roomData.address}
+        </div>
+
+        <div>
+          Web: <a href={website}>{roomData.website}</a>
+        </div>
+      </section>
 
     )
   }
